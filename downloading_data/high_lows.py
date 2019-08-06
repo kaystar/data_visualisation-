8a6499c9c120f72728eadc8c7cced29b8eb6e463
Kevin Rose <k.a.wood91@gmail.com>
2019-08-06 15:54:30 -0400

    Project completed

--- downloading_data/high_lows.py
+++ downloading_data/high_lows.py
@@ -1,47 +1,54 @@
 import csv
-from matplotlib import pyplot as plt
 from datetime import datetime
 
+from matplotlib import pyplot as plt
 
-# Get dates and high temperatures from file
-filename = 'death_valley_2014.csv'
+def get_weather_data(filename, dates, highs, lows):
+    """Get the highs and lows from a data file."""
+    with open(filename) as f:
+        reader = csv.reader(f)
+        header_row = next(reader)
 
-with open(filename) as f:
-    reader = csv.reader(f)
-    header_row = next(reader)
-    print(header_row)
+        # dates, highs, lows = [], [], []
+        for row in reader:
+            try:
+                current_date = datetime.strptime(row[0], "%Y-%m-%d")
+                high = int(row[1])
+                low = int(row[3])
+            except ValueError:
+                print(current_date, 'missing data')
+            else:
+                dates.append(current_date)
+                highs.append(high)
+                lows.append(low)
 
-    dates, highs, lows = [], [], []
-    for row in reader:
-        try:
-            current_date = datetime.strptime(row[0], "%Y-%m-%d")
-            high = int(row[1])
-            low = int(row[3])
-        except ValueError:
-            print(current_date, 'missing data')
+# Get weather data for Sitka.
+dates, highs, lows = [], [], []
+get_weather_data('sitka_weather_2014.csv', dates, highs, lows)
 
-        else:
-            dates.append(current_date)
-            highs.append(high)
-            lows.append(low)
+# Plot Sitka weather data.
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dates, highs, c='red', alpha=0.6)
+plt.plot(dates, lows, c='blue', alpha=0.6)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.15)
 
-    # Plot data
-    fig = plt.figure(dpi=128, figsize=(10, 6))
-    plt.plot(dates, highs, c='red', alpha=0.5)
-    plt.plot(dates, lows, c='blue', alpha=0.5)
-    plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+# Get Death Valley data.
+dates, highs, lows = [], [], []
+get_weather_data('death_valley_2014.csv', dates, highs, lows)
 
-    # Format Plot
-    plt.title("Daily high and low temperatures, 2014\nDeath Valley", fontsize =24)
-    plt.xlabel('', fontsize=20)
-    fig.autofmt_xdate()
-    plt.ylabel("Temperature (F)", fontsize=16)
-    plt.tick_params(axis='both', which='major', labelsize=16)
+# Add Death Valley data to current plot.
+plt.plot(dates, highs, c='red', alpha=0.3)
+plt.plot(dates, lows, c='blue', alpha=0.3)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.05)
 
-    plt.show()
-    # print(highs)
+# Format plot.
+title = "Daily high and low temperatures - 2014"
+title += "\nSitka, AK and Death Valley, CA"
+plt.title(title, fontsize=20)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+plt.ylim(10, 120)
 
-
-
-# for index, column in enumerate(header_row):
-#     print(index, column)
+plt.show()
